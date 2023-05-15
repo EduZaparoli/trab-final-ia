1999d783e05ec23dbf1913ad97f70b382f94e356
Eduardo Zaparoli <eduzzx1204@gmail.com>
2023-05-15 18:54:51 -0300

    trab-ia

---
+++ analisetxt.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+
+# Carrega o dataset IMDB
+max_features = 5000
+maxlen = 400
+batch_size = 32
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+
+# Obtém o índice de palavras do dataset
+word_index = imdb.get_word_index()
+word_index = {k: (v + 3) for k, v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+
+# Inverte o índice de palavras
+index_to_word = {v: k for k, v in word_index.items()}
+
+# Define o modelo de rede neural
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(max_features, 64),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Conv1D(64, 5, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=4),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Treina o modelo
+model.fit(x_train, y_train, batch_size=batch_size, epochs=5, validation_data=(x_test, y_test))
+
+# Salvar os pesos do modelo
+model.save_weights('modelo_pesos.h5')
+
+# Salvar a arquitetura do modelo em formato JSON
+with open('modelo_arquitetura.json', 'w') as json_file:
+    json_file.write(model.to_json())
+
+# Salvar o índice de palavras
+import json
+
+with open('word_index.json', 'w') as json_file:
+    json.dump(word_index, json_file)
+
+# Salvar o índice inverso de palavras
+with open('index_to_word.json', 'w') as json_file:
+    json.dump(index_to_word, json_file)
---
+++ modelo_treinado.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import sequence
+
+import json
+
+maxlen = 400  # Defina o valor correto do comprimento máximo dos textos
+
+# Carregar a arquitetura do modelo
+with open('modelo_arquitetura.json', 'r') as json_file:
+    model_json = json_file.read()
+loaded_model = model_from_json(model_json)
+
+# Carregar os pesos do modelo
+loaded_model.load_weights('modelo_pesos.h5')
+
+# Carregar o índice de palavras
+with open('word_index.json', 'r') as json_file:
+    word_index = json.load(json_file)
+
+# Carregar o índice inverso de palavras
+with open('index_to_word.json', 'r') as json_file:
+    index_to_word = json.load(json_file)
+
+while True:
+    # Solicita entrada do usuário
+    text = input("Digite o texto (ou 'sair' para encerrar): ")
+
+    # Verifica se o usuário deseja sair
+    if text.lower() == 'sair':
+        break
+
+    # Pré-processa o texto
+    text_vector = sequence.pad_sequences([[word_index[word] for word in text.lower().split()]], maxlen=maxlen)
+
+    # Faz a previsão
+    prediction = loaded_model.predict(text_vector)[0][0]
+
+    # Interpreta a previsão
+    if prediction >= 0.5:
+        print("O sentimento associado ao texto é positivo.")
+    else:
+        print("O sentimento associado ao texto é negativo.")
+
